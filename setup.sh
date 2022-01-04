7b927f08877ade9c1a826807e7f6bd03c32714c7
Tomás Migone <tomasmigone@gmail.com>
2022-01-04 14:47:46 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ brew install --cask ngrok
 
 # Install programming languages
 echo "- Installing programming languages"
-brew install python nvm node typescript
+brew install python nvm node typescript go
 
 # Install CLI tools
 echo "- Installing CLI tools"
