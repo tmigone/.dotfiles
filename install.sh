838a410a13f66134a7d5f42495999e6fd54f8906
Tomás Migone <tomasmigone@gmail.com>
2022-09-06 14:45:01 +0200

    Add karabiner
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
-STOW_PACKAGES=('tmux' 'zsh' 'oh-my-zsh' 'git' 'bin')
+STOW_PACKAGES=('tmux' 'zsh' 'oh-my-zsh' 'git' 'bin' 'karabiner')
 
+# Ensure no stow conflicts
+rm ~/.config/karabiner/karabiner.json
+
+# stow
 for i in "${STOW_PACKAGES[@]}"
 do
   echo "Stowing package $i"
